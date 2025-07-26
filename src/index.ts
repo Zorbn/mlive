d897b3d7c641e62de05b7600588b03c346d1ad5b
Zorbn <public.zorb@gmail.com>
2025-07-26 00:04:06 -0500

    Support copying code into links

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,76 @@ import { MError } from "./mError.js";
 
 const inputTextArea = document.getElementById("inputTextArea") as HTMLTextAreaElement;
 const evaluateButton = document.getElementById("evaluateButton") as HTMLButtonElement;
+const copyLinkButton = document.getElementById("copyLinkButton") as HTMLButtonElement;
 const outputTextArea = document.getElementById("outputTextArea") as HTMLTextAreaElement;
+
+inputTextArea.value = `    write !,"hi"
+
+SCRIPT
+    QUIT
+
+main n var1,var2,varArr
+    s var1="a",var2="b",varArr(1)="a",varArr(2)="b",varArr(3,"hello")="hello there",varArr(3,"hi")="hi there"
+    d printArrayKeys(.varArr)
+    d printArray1and2(varArr)
+    d printArray1and2(.varArr)
+    w !,"Var 1: ",var1,", Var 2: ",var2
+    d updateVars(.var1,var2)
+    w !,"Var 1: ",var1,", Var 2: ",var2
+    d updateVars(var1,.var2)
+    w !,"Var 1: ",var1,", Var 2: ",var2
+    w !,"VarArr 1: ",varArr(1),", VarArr 2: ",varArr(2)
+    w !,"VarArr 3, Hi: ",varArr(3,"hi"),", VarArr 3, Hello: ",varArr(3,"hello")
+    w !,"After VarArr(1) is VarArr(",$O(varArr(1)),")"
+    w !,"After VarArr(3, ""hello"") is VarArr(""",$O(varArr(3,"hello")),""")"
+    wRIte !,"Hello, world"
+    d myOtherFunction()
+    w !,"Result of identity on 123 is: """,$$identity(123),"""!!!"
+    w !,"Result of my function is: ",$$myFunction() d
+    . d  w !,"In the block 1"
+    . . w !,"In the inner block 1"
+    . . w !,"In the inner block 2"
+    . w !,"In the block 2",!,"Still in the block"
+    d
+    . w !,"This will be executed"
+    . q
+    . w !,"And this won't"
+    i 100=100,1,3'>-2 d  w !,"The first condition is true"
+    . w !,"Hi from within condition 1"
+    e  w !,"The first condition is false"
+    i 77.7=77.7,430>123,'0 d  w !,"The second condition is true"
+    . w !,"Hi from within condition 2"
+    e  w !,"The second condition is false"
+    w !,"Done with main!"
+    q
+
+printArray1and2(array) w !,"Array 1: ",array(1) d printArray2(.array) q
+
+printArray2(array) w !,"Array 2: ",array(2) q
+
+updateVars(firstVar,secondVar)
+    s firstVar="updated a"
+    s secondVar="update b"
+    q
+
+myFunction()
+    q 1+999-500-100
+
+myOtherFunction()
+    w !,"In the other function"
+    q 777
+
+printArrayKeys(array)
+    n key
+    f  s key=$O(array(key)) q:key=""  w !,"Key: ",key
+    q
+
+identity(x)
+    q x`;
+
+if (window.location.search.length > 1) {
+    inputTextArea.value = atob(window.location.search.slice(1));
+}
 
 const insertIntoTextArea = (text: string, textArea: HTMLTextAreaElement) => {
     const textBefore = textArea.value.slice(0, textArea.selectionStart);
@@ -70,70 +139,6 @@ inputTextArea.addEventListener("keydown", (event) => {
     inputTextArea.selectionStart = inputTextArea.selectionEnd = indentStart;
 });
 
-inputTextArea.value = `    write !,"hi"
-
-SCRIPT
-    QUIT
-
-main n var1,var2,varArr
-    s var1="a",var2="b",varArr(1)="a",varArr(2)="b",varArr(3,"hello")="hello there",varArr(3,"hi")="hi there"
-    d printArrayKeys(.varArr)
-    d printArray1and2(varArr)
-    d printArray1and2(.varArr)
-    w !,"Var 1: ",var1,", Var 2: ",var2
-    d updateVars(.var1,var2)
-    w !,"Var 1: ",var1,", Var 2: ",var2
-    d updateVars(var1,.var2)
-    w !,"Var 1: ",var1,", Var 2: ",var2
-    w !,"VarArr 1: ",varArr(1),", VarArr 2: ",varArr(2)
-    w !,"VarArr 3, Hi: ",varArr(3,"hi"),", VarArr 3, Hello: ",varArr(3,"hello")
-    w !,"After VarArr(1) is VarArr(",$O(varArr(1)),")"
-    w !,"After VarArr(3, ""hello"") is VarArr(""",$O(varArr(3,"hello")),""")"
-    wRIte !,"Hello, world"
-    d myOtherFunction()
-    w !,"Result of identity on 123 is: """,$$identity(123),"""!!!"
-    w !,"Result of my function is: ",$$myFunction() d
-    . d  w !,"In the block 1"
-    . . w !,"In the inner block 1"
-    . . w !,"In the inner block 2"
-    . w !,"In the block 2",!,"Still in the block"
-    d
-    . w !,"This will be executed"
-    . q
-    . w !,"And this won't"
-    i 100=100,1,3'>-2 d  w !,"The first condition is true"
-    . w !,"Hi from within condition 1"
-    e  w !,"The first condition is false"
-    i 77.7=77.7,430>123,'0 d  w !,"The second condition is true"
-    . w !,"Hi from within condition 2"
-    e  w !,"The second condition is false"
-    w !,"Done with main!"
-    q
-
-printArray1and2(array) w !,"Array 1: ",array(1) d printArray2(.array) q
-
-printArray2(array) w !,"Array 2: ",array(2) q
-
-updateVars(firstVar,secondVar)
-    s firstVar="updated a"
-    s secondVar="update b"
-    q
-
-myFunction()
-    q 1+999-500-100
-
-myOtherFunction()
-    w !,"In the other function"
-    q 777
-
-printArrayKeys(array)
-    n key
-    f  s key=$O(array(key)) q:key=""  w !,"Key: ",key
-    q
-
-identity(x)
-    q x`;
-
 const handleErrors = (errors: MError[]) => {
     if (errors.length === 0) {
         return false;
@@ -157,4 +162,9 @@ evaluateButton.addEventListener("click", () => {
         console.log(`Evaluation completed successfully in ${end - start}ms!`);
         outputTextArea.value = result.output;
     }
+});
+
+copyLinkButton.addEventListener("click", () => {
+    const encodedScript = btoa(inputTextArea.value);
+    navigator.clipboard.writeText(`${window.location.origin}/?${encodedScript}`);
 });
